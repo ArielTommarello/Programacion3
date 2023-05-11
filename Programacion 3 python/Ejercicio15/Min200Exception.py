d1fe3b3fcdf3968ef52c2904753a903955de4c8f
arieltommarello <ariel.tommarello@gmail.com>
2023-05-11 16:42:59 -0300

    Cambios total

---
+++ Programacion 3 python/Ejercicio15/Min200Exception.py
@@ -0,0 +1,7 @@
+class EmptyBatteryException (Exception):
+    def __init__(self, message, code):
+            self.__message = message
+            self.__code = code
+    def __str__(self):        
+
+        return f'Error: {self.__code}. {self.__message}'
